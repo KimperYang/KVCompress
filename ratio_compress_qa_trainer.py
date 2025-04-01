718518a9173ad68132b8bc9b3a77f04eb00a47c5
kimperyang <jingbo@ucsb.edu>
2025-04-01 03:41:22 +0000

    update

--- ratio_compress_qa_trainer.py
+++ ratio_compress_qa_trainer.py
@@ -76,9 +76,9 @@ def main():
     compress_tokens = list(range(128011, 128211))
     ratio = 0.5
 
-    global_tokenizer = AutoTokenizer.from_pretrained("training_res/ratio_compress_multichunk20k/checkpoint-20000")
+    global_tokenizer = AutoTokenizer.from_pretrained("training_res/ratio_50_compress_multichunk20k_kvlink/checkpoint-20000")
     global_model = AutoModelForCausalLM.from_pretrained(
-        "training_res/ratio_compress_multichunk20k/checkpoint-20000",
+        "training_res/ratio_50_compress_multichunk20k_kvlink/checkpoint-20000",
         torch_dtype=torch.bfloat16,
         attn_implementation='sdpa',
         # use_flash_attention_2=True,
@@ -101,9 +101,9 @@ def main():
     os.environ["WANDB_WATCH"]="false"
 
     training_args = TrainingArguments(
-        output_dir="training_res/ratio_compress_qa_kvlink_multichunk20k",
+        output_dir="training_res/ratio_compress_qa_kvlink_linkchunk20k",
         report_to="wandb",
-        run_name=f"ratio_compress_qa_kvlink_{int(len(compress_tokens) * 100)}_multichunk20k",
+        run_name=f"ratio_compress_qa_kvlink_{int(len(compress_tokens) * 100)}_linkchunk20k",
         per_device_train_batch_size= batch_size_per_device,
         num_train_epochs=2,
         logging_dir="training_res/logs",
--- train.sh
+++ train.sh
@@ -1,24 +1,2 @@
-#!/bin/bash
-
-#SBATCH --job-name=train_generator
-#SBATCH -D .
-#SBATCH --output=/data/jingbo_yang/KVMemory/sbatch/O-%x.%j
-#SBATCH --error=/data/jingbo_yang/KVMemory/sbatch/E-%x.%j
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1         # number of MP tasks
-#SBATCH --gres=gpu:8           # number of GPUs per node
-#SBATCH --cpus-per-task=128         # number of cores per tasks
-#SBATCH --time=48:00:00             # maximum execution time (HH:MM:SS)
-#SBATCH --signal=SIGUSR1@90     #Signal is sent 90s before time expires
-######################
-### Set enviroment ###
-######################
-
-
-source /data/jingbo_yang/.bashrc
-conda activate kvm
-cd /data/jingbo_yang/KVCompress
-
-
-# accelerate launch --config_file /data/jingbo_yang/.cache/huggingface/accelerate/step2.yaml --main_process_port 25671 compress_pretraining_trainer.py
-accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_pretraining_trainer.py
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_pretraining_trainer.py
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_qa_trainer.py
