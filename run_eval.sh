053b40fa646aff02f61d4ebc1f3bc41961b58a4a
kimperyang <jingbo@ucsb.edu>
2025-04-05 07:18:23 +0000

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,10 +1,10 @@
 conda activate kvm
 # cd /data/jingbo_yang/KVCompress
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/kvlink/tqa_eval.py --run "block_qa" --ckpt 1122 &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/kvlink/hqa2_eval.py --run "block_qa" --ckpt 1122 &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/kvlink/nq2_eval.py --run "block_qa" --ckpt 1122 &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/kvlink/wiki_eval.py --run "block_qa" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/kvlink/tqa_eval.py --run "kvlink_qa_multichunk20k" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/kvlink/hqa2_eval.py --run "kvlink_qa_multichunk20k" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/kvlink/nq2_eval.py --run "kvlink_qa_multichunk20k" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/kvlink/wiki_eval.py --run "kvlink_qa_multichunk20k" --ckpt 1122 &
 # CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/upper/tqa_upper.py --run "upper_2epoch" --ckpt 1122 &
 # CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/upper/nq2_upper.py --run "upper_2epoch" --ckpt 1122 &
 # CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/upper/hqa2_upper.py --run "upper_2epoch" --ckpt 1122 &
