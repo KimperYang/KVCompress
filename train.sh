f2c4b8a4ab7cebf3da0a9b15a2cd6423b7343470
kimperyang <jingbo@ucsb.edu>
2025-04-06 18:20:29 +0000

    update

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-python preprocess.py
+# python preprocess.py
 accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_pretraining_trainer.py
 
 # accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_qa_trainer.py
