e7478954e28c979beb6b9e06fedd828711d2f948
kimperyang <jingbo@ucsb.edu>
2025-04-06 21:17:49 +0000

    update

--- compress_qa_trainer.py
+++ compress_qa_trainer.py
@@ -81,11 +81,11 @@ def load_from_disk_then_process(
 
 def main():
     batch_size_per_device = 4
-    compress_tokens = list(range(128011, 128061))
+    compress_tokens = list(range(128011, 128031))
 
-    global_tokenizer = AutoTokenizer.from_pretrained("training_res/compress_chunk_pretrain_multichunk20k/checkpoint-20000")
+    global_tokenizer = AutoTokenizer.from_pretrained("training_res/compress_chunk20_pretrain_multichunk20k/checkpoint-20000")
     global_model = AutoModelForCausalLM.from_pretrained(
-        "training_res/compress_chunk_pretrain_multichunk20k/checkpoint-20000",
+        "training_res/compress_chunk20_pretrain_multichunk20k/checkpoint-20000",
         torch_dtype=torch.bfloat16,
         attn_implementation='sdpa',
         # use_flash_attention_2=True,
@@ -106,9 +106,9 @@ def main():
     os.environ["WANDB_WATCH"]="false"
 
     training_args = TrainingArguments(
-        output_dir="training_res/compress_chunk_qa_kvlink_nopadding_multichunk20k_epoch2",
+        output_dir="training_res/compress_chunk20_qa_kvlink_nopadding_multichunk20k_epoch2",
         report_to="wandb",
-        run_name=f"compress_chunk_{len(compress_tokens)}_qa_kvlink_nopadding_multichunk20k_epoch2_bsz{batch_size_per_device}",
+        run_name=f"compress_chunk20_{len(compress_tokens)}_qa_kvlink_nopadding_multichunk20k_epoch2_bsz{batch_size_per_device}",
         per_device_train_batch_size= batch_size_per_device,
         num_train_epochs=2,
         # max_steps=2500,
--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 # python preprocess.py
-accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_pretraining_trainer.py
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_qa_trainer.py
 
 # accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_qa_trainer.py
 # CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/ratio_kvlink/tqa_eval.py --run "ratio_compress_qa_kvlink_linkchunk20k" --ckpt 1122 &
