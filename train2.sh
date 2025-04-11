c9ce3ad7ba23aa0f6df0b4bccb8d18eae3249c26
kimperyang <jingbo@ucsb.edu>
2025-04-11 03:45:28 +0000

    update

--- train2.sh
+++ train2.sh
@@ -1,5 +1,5 @@
 # python preprocess.py
-accelerate launch --config_file config/step2.yaml --main_process_port 25671 kvlink_qa_trainer.py
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_qa_trainer.py
 # CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/kvlink/tqa_eval.py --run "block_qa" --ckpt 1122 &
 # CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/kvlink/hqa2_eval.py --run "block_qa" --ckpt 1122 &
 # CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/kvlink/nq2_eval.py --run "block_qa" --ckpt 1122 &
