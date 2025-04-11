65a5ce31e726e410fcc0c69ad6ef503e3b05ab3d
kimperyang <jingbo@ucsb.edu>
2025-04-11 07:33:40 +0000

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,12 +1,12 @@
 conda activate kvm
 # cd /data/jingbo_yang/KVCompress
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/kvlink/tqa_eval.py --run "kvlink_qa" --ckpt 1122 &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/kvlink/hqa2_eval.py --run "kvlink_qa" --ckpt 1122 &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/kvlink/nq2_eval.py --run "kvlink_qa" --ckpt 1122 &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/kvlink/wiki_eval.py --run "kvlink_qa" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/ratio/tqa_eval.py --run "compress_chunk20_qa_kvlink_nopadding_multichunk20k_epoch2" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/ratio/nq2_eval.py --run "compress_chunk20_qa_kvlink_nopadding_multichunk20k_epoch2" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/ratio/hqa2_eval.py --run "compress_chunk20_qa_kvlink_nopadding_multichunk20k_epoch2" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/ratio/wiki_eval.py --run "compress_chunk20_qa_kvlink_nopadding_multichunk20k_epoch2" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/ratio_kvlink/tqa_eval.py --run "ratio_50_compress_qa_kvlink_multichunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/ratio_kvlink/hqa2_eval.py --run "ratio_50_compress_qa_kvlink_multichunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/ratio_kvlink/nq2_eval.py --run "ratio_50_compress_qa_kvlink_multichunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/ratio_kvlink/wiki_eval.py --run "ratio_50_compress_qa_kvlink_multichunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/ratio_kvlink/tqa_eval.py --run "ratio_50_compress_qa_kvlink_linkchunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/ratio_kvlink/nq2_eval.py --run "ratio_50_compress_qa_kvlink_linkchunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/ratio_kvlink/hqa2_eval.py --run "ratio_50_compress_qa_kvlink_linkchunk20k_1e-5" --ckpt 1122 &
+CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/ratio_kvlink/wiki_eval.py --run "ratio_50_compress_qa_kvlink_linkchunk20k_1e-5" --ckpt 1122 &
 wait
