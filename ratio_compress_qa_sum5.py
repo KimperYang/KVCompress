301a3f731a055660e6c2b13606df66e482ad94f1
kimperyang <jingbo@ucsb.edu>
2025-04-11 21:28:57 +0000

    update

--- ratio_compress_qa_sum5.py
+++ ratio_compress_qa_sum5.py
@@ -1,6 +1,6 @@
 """
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --config_file /data/jingbo_yang/.cache/huggingface/accelerate/step2.yaml \
---main_process_port 25671 ratio_compress_mix_trainer.py
+--main_process_port 25671 ratio_compress_pretraining_trainer.py
 """
 import os
 from typing import Tuple
@@ -36,16 +36,26 @@ def load_from_disk_then_process(
             "file_path", "language", "language_score", "token_count",
         ]
         num_shards = 512
+    if data_component_name in ["qa_compress", "qa_compress_link"]:
+        data_path = "dataset_cache/processed/compress_qa"
+        if data_component_name == "qa_compress":
+            preprocessor_fn = preprocessor.process_qa_compress
+        elif data_component_name == "qa_compress_link":
+            preprocessor_fn = preprocessor.process_qa_compress_kvlink
+        else:
+            raise NotImplementedError()
+        remove_columns=['prompt', 'question', 'answers', 'generated', 'inputs', 'documents']
+        num_shards = 512
     else:
         raise NotImplementedError()
     data_component: datasets.DatasetDict = datasets.load_from_disk(data_path)
 
-    streaming_train_dataset = data_component["train"].to_iterable_dataset(num_shards=num_shards)
-    # streaming_train_dataset = data_component["train"]
+    # streaming_train_dataset = data_component["train"].to_iterable_dataset(num_shards=num_shards)
+    streaming_train_dataset = data_component["train"]
     training_data = streaming_train_dataset.map(
         preprocessor_fn,
         remove_columns=remove_columns,
-        # num_proc=16,
+        num_proc=16,
         batched=False,
     )
 
@@ -65,66 +75,60 @@ def main():
     batch_size_per_device = 4
     compress_tokens = list(range(128011, 128211))
     ratio = 0.5
+    # compress_tokens = list(range(128011, 128091))
+    # ratio = 0.2
 
-    global_tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
+    global_tokenizer = AutoTokenizer.from_pretrained("training_res/ratio_compress_multichunk20k/checkpoint-20000")
     global_model = AutoModelForCausalLM.from_pretrained(
-        "meta-llama/Llama-3.2-1B",
+        "training_res/ratio_compress_multichunk20k/checkpoint-20000",
         torch_dtype=torch.bfloat16,
         attn_implementation='sdpa',
         # use_flash_attention_2=True,
     )
+
+    new_special_tokens = [f"<|reserved_special_token_{248+i}|>" for i in range(100)]
+    special_tokens_dict = {"additional_special_tokens": new_special_tokens}
+
+    global_tokenizer.add_special_tokens(special_tokens_dict, replace_additional_special_tokens=False)
+    global_model.resize_token_embeddings(len(global_tokenizer))
 
     preprocessor = compress_ratio_preprocessor(
         tokenizer=global_tokenizer,
         max_len=4096,
         compress_tokens=compress_tokens,
         compress_ratio=ratio,
-        chunk_end_token=128253,
-        do_shuffle=True
-    )
-    
-    singlechunk_train, singlechunk_eval = load_from_disk_then_process("text_singlechunk", preprocessor)
-    multichunk_train, multichunk_eval = load_from_disk_then_process("text_multichunk", preprocessor)
-
-    train_dataset = datasets.interleave_datasets(
-        [singlechunk_train, multichunk_train],
-        probabilities=[0.50, 0.50],
-        seed=42,
-        stopping_strategy="all_exhausted",
+        chunk_end_token=128005,
+        global_start_token=128002,
+        global_end_token=128003,
+        do_shuffle=True,
+        link_token_num = 5,
+        max_chunk_num = 10,
     )
 
-    eval_dataset = datasets.DatasetDict({
-        "single_chunk": singlechunk_eval,
-        "multi_chunk": multichunk_eval,
-    })
+    train_dataset, eval_dataset = load_from_disk_then_process("qa_compress_link", preprocessor)
 
     os.environ["WANDB_PROJECT"]="kvcompress"
     os.environ["WANDB_WATCH"]="false"
 
     training_args = TrainingArguments(
-        output_dir=f"training_res/ratio_compress_pretrain_mix",
+        output_dir=f"training_res/ratio_{int(ratio * 100)}_compress_qa_kvlink5_multichunk20k_2e-5",
         report_to="wandb",
-        run_name=f"ratio_compress_{int(len(compress_tokens) * 100)}_pretrain_mix",
+        run_name=f"ratio_{int(ratio * 100)}_compress_qa_kvlink5_multichunk20k_2e-5",
         per_device_train_batch_size= batch_size_per_device,
-        # num_train_epochs=2,
-        max_steps=20000,
+        num_train_epochs=2,
         logging_dir="training_res/logs",
         logging_steps=10,
-        save_steps=4000,
         gradient_accumulation_steps=2,
         warmup_ratio=0.1,
         lr_scheduler_type='cosine',
         bf16=True,
-        learning_rate=5e-6,
+        learning_rate=2e-5,
         do_eval=True,
         per_device_eval_batch_size = batch_size_per_device,
-        evaluation_strategy="steps",  # Add this line
-        eval_steps=1000,
+        evaluation_strategy="epoch",  # Add this line
         gradient_checkpointing=True,
         save_total_limit=1,
-        # overwrite_output_dir = False
         remove_unused_columns=False,
-        # split_batches=True,
         dispatch_batches=False,
         eval_on_start=True,
         seed = 42
