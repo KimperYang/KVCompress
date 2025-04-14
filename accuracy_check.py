18aefe575d4e56aaeb0c862224056cc31f3b85cf
kimperyang <jingbo@ucsb.edu>
2025-04-14 00:05:11 +0000

    update

--- accuracy_check.py
+++ accuracy_check.py
@@ -1,16 +1,6 @@
-import os
-import torch
-import string
-import argparse
 import json
-import regex
-import datasets
-import math
+import math 
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import numpy as np
-
-from typing import List
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def do_stats(dataset_name, tokenizer):
@@ -36,24 +26,25 @@ def do_stats(dataset_name, tokenizer):
         with open(file_path, 'r') as file:
             data = [json.loads(line) for line in file]
 
-    # context_len = []
+    context_len = []
     context_num = []
-    chunk_num_list = []
-    doc_length_list = []
-
+    exceed_chunk_num_index = []
     for i in tqdm(range(len(data))):
 
         if dataset_name == "hqa":
 
             context_num.append(len(data[i]['documents']))
+            accumulated_len = 0
             chunk_num = 0
             for j in range(len(data[i]['documents'])):
                 title = data[i]['documents'][j]['title']
                 text = data[i]['documents'][j]['text']
                 tem_id = tokenizer(f"Document [{j+1}](Title: {title}) {text}\n", add_special_tokens=False).input_ids
-                doc_length_list.append(len(tem_id))
+                accumulated_len += len(tem_id)
                 chunk_num += math.ceil(len(tem_id) / 100)
-            chunk_num_list.append(chunk_num)
+            if chunk_num > 20:
+                exceed_chunk_num_index.append(i)
+            context_len.append(accumulated_len / len(data[i]['documents']))
         
         elif dataset_name == "tqa":
 
@@ -64,10 +55,11 @@ def do_stats(dataset_name, tokenizer):
                 title = data[i]['documents'][j]['title']
                 text = data[i]['documents'][j]['text']
                 tem_id = tokenizer(f"Document [{j+1}](Title: {title}) {text}\n", add_special_tokens=False).input_ids
-                doc_length_list.append(len(tem_id))
+                accumulated_len += len(tem_id)
                 chunk_num += math.ceil(len(tem_id) / 100)
-            chunk_num_list.append(chunk_num)
-            
+            if chunk_num > 20:
+                exceed_chunk_num_index.append(i)
+            context_len.append(accumulated_len / len(data[i]['documents']))
 
         elif dataset_name == "wiki":
 
@@ -78,9 +70,11 @@ def do_stats(dataset_name, tokenizer):
                 title = data[i]['context'][j][0]
                 text = " ".join(data[i]['context'][j][1])
                 tem_id = tokenizer(f"Document [{j+1}](Title: {title}) {text}\n", add_special_tokens=False).input_ids
-                doc_length_list.append(len(tem_id))
+                accumulated_len += len(tem_id)
                 chunk_num += math.ceil(len(tem_id) / 100)
-            chunk_num_list.append(chunk_num)
+            if chunk_num > 20:
+                exceed_chunk_num_index.append(i)
+            context_len.append(accumulated_len / len(data[i]['context']))
 
         elif dataset_name == "nq":
 
@@ -91,10 +85,11 @@ def do_stats(dataset_name, tokenizer):
                 title = data[i]['documents'][j]['title']
                 text = data[i]['documents'][j]['text']
                 tem_id = tokenizer(f"Document [{j+1}](Title: {title}) {text}\n", add_special_tokens=False).input_ids
-                doc_length_list.append(len(tem_id))
+                accumulated_len += len(tem_id)
                 chunk_num += math.ceil(len(tem_id) / 100)
-            chunk_num_list.append(chunk_num)
-            
+            if chunk_num > 20:
+                exceed_chunk_num_index.append(i)
+            context_len.append(accumulated_len / len(data[i]['documents']))
         
         elif dataset_name == "train":
 
@@ -105,70 +100,35 @@ def do_stats(dataset_name, tokenizer):
                 title = data[i]['documents'][j]['title']
                 text = data[i]['documents'][j]['text']
                 tem_id = tokenizer(f"Document [{j+1}](Title: {title}) {text}\n", add_special_tokens=False).input_ids
-                doc_length_list.append(len(tem_id))
+                accumulated_len += len(tem_id)
                 chunk_num += math.ceil(len(tem_id) / 100)
-            chunk_num_list.append(chunk_num)
-            
+            if chunk_num > 20:
+                exceed_chunk_num_index.append(i)
+            context_len.append(accumulated_len / len(data[i]['documents']))
 
     # if dataset_name == "train":
-    return chunk_num_list, doc_length_list
-    # return sum(context_num) / len(context_num), sum(context_len) / len(context_len)
+    return exceed_chunk_num_index
+
+data_path1="result/compress_chunk_qa_kvlink_nopadding_multichunk20k_epoch2/wiki_full_ckpt1122_0.7232029262086515.jsonl"
+data_path2="result/compress_chunk_qa_nopadding_multichunk20k_epoch2/wiki_full_ckpt1122_0.7176367684478372.jsonl"
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
 
+exceed_index = do_stats("wiki", tokenizer)
+with open(data_path1, 'r') as file:
+    data1 = [json.loads(line) for line in file]
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, required=True, help='Checkpoint number')
-args = parser.parse_args()
-dataset_name = args.dataset
+with open(data_path2, 'r') as file:
+    data2 = [json.loads(line) for line in file]
 
-chunk_num_list, doc_length_list = do_stats(dataset_name, tokenizer)
-# print("Max chunk num: ", max(chunk_num_list))
-# print("Avg chunk num: ", sum(chunk_num_list) / len(chunk_num_list))
+print(exceed_index[0], len(exceed_index))
 
+total_score_data1 = 0
+total_score_data2 = 0
 
-# data = chunk_num_list
-# # Compute statistics for reference (optional)
-# mean_val = np.mean(data)
-# median_val = np.median(data)
+for idx in exceed_index:
+    total_score_data1 += data1[idx]['Score']
+    total_score_data2 += data2[idx]['Score']
 
-# # Set up bins so each integer has its own bin
-# # +2 on max(data) because range() is exclusive at the upper boundary
-# bins = range(min(data), max(data) + 2)
-
-# plt.hist(data, bins=bins, align='left', alpha=0.8)
-# plt.axvline(mean_val, linestyle='--', label=f"Mean = {mean_val:.2f}")
-# # plt.axvline(median_val, linestyle=':', label=f"Median = {median_val:.2f}")
-# plt.axvline(max(data), linestyle=':', label=f"Max = {max(data):.2f}")
-
-# plt.title("Distribution of chunk num")
-# plt.xlabel("Chunk number")
-# plt.ylabel("Data number")
-# plt.grid(True)
-# plt.legend()
-# plt.savefig(f"figs/{dataset_name}_500.png")  
-
-print("Max doc length: ", max(doc_length_list))
-print("Avg doc length: ", sum(doc_length_list) / len(doc_length_list))
-
-
-data = doc_length_list
-# Compute statistics for reference (optional)
-mean_val = np.mean(data)
-median_val = np.median(data)
-
-# Set up bins so each integer has its own bin
-# +2 on max(data) because range() is exclusive at the upper boundary
-bins = range(min(data), max(data) + 2)
-
-plt.hist(data, bins=bins, align='left', alpha=0.8)
-plt.axvline(mean_val, linestyle='--', label=f"Mean = {mean_val:.2f}")
-# plt.axvline(median_val, linestyle=':', label=f"Median = {median_val:.2f}")
-plt.axvline(max(data), linestyle=':', label=f"Max = {max(data):.2f}")
-
-plt.title("Distribution of Doc Length")
-plt.xlabel("Document token length")
-plt.ylabel("Frequency")
-plt.grid(True)
-plt.legend()
-plt.savefig(f"figs/{dataset_name}.png")  
+print("With kvlink accuracy: ", total_score_data1 / len(exceed_index))
+print("Without kvlink accuracy: ", total_score_data2 / len(exceed_index))
