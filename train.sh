de1887e6bca4768991d461762672e7ae95f0cebf
kimperyang <jingbo@ucsb.edu>
2025-04-17 07:52:36 +0000

    add traine

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 # python preprocess.py
 accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_qa_trainer.py
-accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_qa_trainer.py --dataset "qa_compress"
-accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_qa_trainer.py --dataset "qa_compress_link"
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_qa_trainer.py --dataset "qa_compress"
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_qa_trainer.py --dataset "qa_compress_link"
