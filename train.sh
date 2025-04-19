41e177a39ee98aff11eddcfb56b5017f5872269f
kimperyang <jingbo@ucsb.edu>
2025-04-19 20:45:02 +0000

    update

--- train.sh
+++ train.sh
@@ -2,14 +2,14 @@
 accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_mix_trainer.py --dataset "qa"
 accelerate launch --config_file config/step2.yaml --main_process_port 25671 compress_mix_trainer.py --dataset "qa_link"
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunk_nopadding_prompt/tqa_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunk_nopadding_prompt/hqa2_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunk_nopadding_prompt/nq2_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunk_nopadding_prompt/wiki_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/chunk_nopadding_kvlink_prompt/tqa_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/chunk_nopadding_kvlink_prompt/nq2_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/chunk_nopadding_kvlink_prompt/hqa2_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/chunk_nopadding_kvlink_prompt/wiki_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 561 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunk_nopadding_prompt/tqa_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunk_nopadding_prompt/hqa2_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunk_nopadding_prompt/nq2_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunk_nopadding_prompt/wiki_eval.py --run "chunkcomp_mix_qa_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/chunk_nopadding_kvlink_prompt/tqa_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/chunk_nopadding_kvlink_prompt/nq2_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/chunk_nopadding_kvlink_prompt/hqa2_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 3000 &
+CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/chunk_nopadding_kvlink_prompt/wiki_eval.py --run "chunkcomp_mix_qa_link_3k" --ckpt 3000 &
 
 wait
 # accelerate launch --config_file config/step2.yaml --main_process_port 25671 ratio_compress_mix_trainer.py --dataset "qa_compress"
