5c7daa919917419cf2ff6f9f1f3d05bce1f70ffe
kimperyang <jingbo@ucsb.edu>
2025-04-29 18:20:59 +0000

    update

--- run1.sh
+++ run1.sh
@@ -4,4 +4,6 @@ startTime=$(date +%s) #mark the start of job
 output_dir=$1
 run_name=$2
 
+conda activate kvm
+
 accelerate launch --config_file config/step2.yaml --main_process_port 25671 chunkaug_ptr_trainer.py
