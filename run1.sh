93aff86d7c81cd67dc1756f28bda577fe7153e65
kimperyang <jingbo@ucsb.edu>
2025-04-29 18:39:02 +0000

    update

--- run1.sh
+++ run1.sh
@@ -4,6 +4,8 @@ startTime=$(date +%s) #mark the start of job
 output_dir=$1
 run_name=$2
 
+export WANDB_API="297fefc6714432e38b47736829a56f96e540206a"
+
 conda activate kvm
 
 accelerate launch --config_file config/step2.yaml --main_process_port 25671 chunkaug_ptr_trainer.py
