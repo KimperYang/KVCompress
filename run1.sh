8ae19c0215b1234dfcde0d3f53d0cbf8a205b8bc
kimperyang <jingbo@ucsb.edu>
2025-04-29 09:07:12 +0000

    update

---
+++ run1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+startTime=$(date +%s) #mark the start of job 
+
+output_dir=$1
+run_name=$2
+
+accelerate launch --config_file config/step2.yaml --main_process_port 25671 chunkaug_ptr_trainer.py
