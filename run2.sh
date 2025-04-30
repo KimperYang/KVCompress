deff5e31e184d71d2c93aa1dbbcdae8eb312f28a
kimperyang <jingbo@ucsb.edu>
2025-04-30 18:29:48 +0000

    update

--- run2.sh
+++ run2.sh
@@ -8,10 +8,10 @@ export WANDB_API_KEY="297fefc6714432e38b47736829a56f96e540206a"
 
 conda activate kvm
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_link/hqa2_eval.py --run "chunkaug_qa_link_only_20k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link/nq2_eval.py --run "chunkaug_qa_link_only_20k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link/tqa_eval.py --run "chunkaug_qa_link_only_20k" --ckpt 561 &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link/wiki_eval.py --run "chunkaug_qa_link_only_20k" --ckpt 561 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_link_prompt/hqa2_eval.py --run "chunkaug_qa_link_20k" --ckpt 561 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link_prompt/nq2_eval.py --run "chunkaug_qa_link_20k" --ckpt 561 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/tqa_eval.py --run "chunkaug_qa_link_20k" --ckpt 561 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/wiki_eval.py --run "chunkaug_qa_link_20k" --ckpt 561 &
 
 
 # accelerate launch --config_file config/step2.yaml --main_process_port 25671 chunkaug_ptr_trainer.py
