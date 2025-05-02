7a93a028986605e8ae75a7ea9a6cda93b3b23993
KimperYang <jingbo@ucsb.edu>
2025-05-02 19:19:46 +0000

    update

--- run5.sh
+++ run5.sh
@@ -8,22 +8,22 @@
 
 conda activate kvm
 
-# CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/hqa2_eval.py --run "chunkaug_qa_20k" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq2_eval.py --run "chunkaug_qa_20k" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/tqa_eval.py --run "chunkaug_qa_20k" --ckpt 1122 &
-# CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/wiki_eval.py --run "chunkaug_qa_20k" --ckpt 1122 &
+# CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/hqa2_eval.py --run "chunkaug_qa_20k" --ckpt 561 &
+# CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq2_eval.py --run "chunkaug_qa_20k" --ckpt 561 &
+# CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/tqa_eval.py --run "chunkaug_qa_20k" --ckpt 561 &
+# CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/wiki_eval.py --run "chunkaug_qa_20k" --ckpt 561 &
 
 
-# python scripts/evaluation/chunkaug_prompt/musique.py --run "chunkaug_10_qa_20k" --ckpt 1122
-python scripts/evaluation/chunkaug_link_prompt/musique.py --run "chunkaug_10_qa_link_continue_20k" --ckpt 1122
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 1
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 2
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 3
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 4
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 5
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 6
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 7
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 8
-# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 1122 --pos 9
+# python scripts/evaluation/chunkaug_prompt/musique.py --run "chunkaug_10_qa_20k" --ckpt 561
+python scripts/evaluation/chunkaug_link_prompt/musique.py --run "chunkaug_10_qa_link_continue_20k" --ckpt 561
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 1
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 2
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 3
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 4
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 5
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 6
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 7
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 8
+# python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_10_qa_20k" --ckpt 561 --pos 9
 
 # accelerate launch --config_file config/step2.yaml --main_process_port 25671 chunkaug_ptr_trainer.py
