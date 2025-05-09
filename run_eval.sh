33c155b255873d00249ea6acbb79806a07ac6343
KimperYang <jingbo@ucsb.edu>
2025-05-09 21:35:28 +0000

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,13 +1,70 @@
 conda activate kvm
 # cd /data/jingbo_yang/KVCompress
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/anllm/tqa.py --run "anchor_5_qa_qa_10k" --ckpt 1122  &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/anllm/hqa.py --run "anchor_5_qa_qa_10k" --ckpt 1122   &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/anllm/musique.py --run "anchor_5_qa_qa_10k" --ckpt 1122   &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/anllm/wiki.py --run "anchor_5_qa_qa_10k" --ckpt 1122   &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/anllm_link/tqa.py --run "anchor_5_freeze_qa_link_10k" --ckpt 561 --reencode 5 &
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/anllm_link/hqa.py --run "anchor_5_freeze_qa_link_10k" --ckpt 561 --reencode 5  &
-CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/anllm_link/musique.py --run "anchor_5_freeze_qa_link_10k" --ckpt 561 --reencode 5  &
-CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/anllm_link/wiki.py --run "anchor_5_freeze_qa_link_10k" --ckpt 561 --reencode 5  &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 0
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 0
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 0
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 0
 
 wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 1
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 1
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 1
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 1
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 2
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 2
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 2
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 2
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 3
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 3
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 3
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 3
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 4
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 4
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 4
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 4
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 5
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 5
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 5
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 5
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 6
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 6
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 6
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 6
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 7
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 7
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 7
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 7
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 8
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 8
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 8
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 8
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 9
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_mix_reencode_0" --ckpt 6000 --pos 9
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link" --ckpt 1122 --pos 9
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_mix_reencode_5" --ckpt 6000 --pos 9
