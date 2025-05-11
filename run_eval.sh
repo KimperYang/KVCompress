cc490bfaaf86708507ade468be1378a93d7d4d45
KimperYang <jingbo@ucsb.edu>
2025-05-11 22:22:06 +0000

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,37 +1,16 @@
 conda activate kvm
 # cd /data/jingbo_yang/KVCompress
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 0
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 0
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 1
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 1
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 2 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 3 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 4 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 5 &
 
 wait
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 2
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 2
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 3
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 3
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 6 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 7 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 8 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa" --ckpt 1122 --pos 9 &
 
-wait
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 4
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 4
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 5
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 5
-
-wait
-
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 6
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 6
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 7
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 7
-
-wait
-
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 8
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 8
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 9
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_link_prompt/nq.py --run "chunkaug_25_qa_link_8gpu" --ckpt 1122 --pos 9
-
-wait
