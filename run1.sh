b050d6071fcf4c21413ad1a60443a14692949776
KimperYang <jingbo@ucsb.edu>
2025-05-11 23:48:21 +0000

    update

--- run1.sh
+++ run1.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-startTime=$(date +%s) #mark the start of job 
+conda activate kvm
+# cd /data/jingbo_yang/KVCompress
 
-output_dir=$1
-run_name=$2
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 2 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 3 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 4 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 5 &
 
-export WANDB_API_KEY="297fefc6714432e38b47736829a56f96e540206a"
+wait
 
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 0
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 1
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 2
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 3
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 4
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 5
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 6
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 7
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 8
-python scripts/evaluation/ratioaug_prompt/nq.py --run "ratioaug_25_qa_compress" --ckpt 1122 --pos 9
-python scripts/evaluation/ratioaug_prompt/hqa.py --run "ratioaug_25_qa_compress" --ckpt 1122
-python scripts/evaluation/ratioaug_prompt/wiki_eval.py --run "ratioaug_25_qa_compress" --ckpt 1122
-python scripts/evaluation/ratioaug_prompt/tqa_eval.py --run "ratioaug_25_qa_compress" --ckpt 1122
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 6 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 7 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 8 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 9 &
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 0 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/chunkaug_prompt/nq.py --run "chunkaug_25_qa_8gpu" --ckpt 1122 --pos 1 &
